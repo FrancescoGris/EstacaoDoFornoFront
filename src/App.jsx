3db44ab3d6dbc62d31ba3f7be3c0b40481da92c7
Francesco Gris <francescofgris@gmail.com>
2026-04-02 21:11:43 -0300

    estrutura base'

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Sidebar from "./componentes/Sidebar";
 import Header from "./componentes/Headers";
 import Home from "./pages/Home";
 import Produtos from "./pages/Produtos";
-
+import Cadastro from './pages/Cadastro';
 import { Routes, Route } from 'react-router-dom';
 
 function App(){
@@ -17,6 +17,7 @@ function App(){
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/produtos" element={<Produtos />} />
+        <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
     </main>
   </div>
